4bdae6b4a2fe7f85a0635b0d4b96ea4988460de8
Juan Camilo <jcamilo.osorio15@gmail.com>
2019-07-12 23:41:08 -0500

    Global variables

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RespuestaMDB } from '../app.interfaces';
+import { environment } from 'src/environments/environment';
+
+const URL = environment.url;
+const apiKey = environment.apiKey;
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +13,16 @@ export class MoviesService {
 
   constructor(private http: HttpClient) { }
 
+  private ejecutarQuery<T>(query:string){
+    query = URL + query + `&api_key=${apiKey}&language=es&include_image_language=es`;
+    return this.http.get(query);
+  }
+
   getFeatures(){
-    return this.http.get<RespuestaMDB>(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&api_key=91a9b78afa14d8c2a57c2ae5798a4cde&language=es&include_image_language=es`);
+    const hoy = new Date();
+    const ultimoDia = new Date(hoy.getFullYear(), hoy.getMonth() + 1, 0).getDate();
+    const mes = hoy.getMonth() + 1;
+    let mesS
+    return this.ejecutarQuery<RespuestaMDB>(`/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22`) 
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: '91a9b78afa14d8c2a57c2ae5798a4cde',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
