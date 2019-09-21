a3ebe8850e7d8350f214cbdff904dda84847f05c
Juan Camilo <jcamilo.osorio15@gmail.com>
2019-09-20 22:27:17 -0500

    movie details finished

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -1,6 +1,7 @@
-import { PeliculaDetalle } from './../../app.interfaces';
+import { PeliculaDetalle, Cast } from './../../app.interfaces';
 import { Component, OnInit, Input } from '@angular/core';
 import { MoviesService } from 'src/app/services/movies.service';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-detalle',
@@ -11,12 +12,27 @@ export class DetalleComponent implements OnInit {
 
   @Input() id;
   pelicula: PeliculaDetalle = {};
+  actores: Cast[] = [];
+  oculto = 150;
+  slideOptActores = {
+    slidesPerView: 3.3,
+    freeMode: true,
+    spaceBetween: -5
+  };
 
-  constructor(private movie:MoviesService) { }
+  constructor(private movie: MoviesService, private modalCtrl: ModalController) { }
 
   ngOnInit() {
     this.movie.getPeliculaDetalle(this.id).subscribe(resp => this.pelicula = resp);
-    this.movie.getActoresPelicula(this.id).subscribe(resp => console.log(resp));
+    this.movie.getActoresPelicula(this.id).subscribe(resp => this.actores = resp.cast);
+  }
+
+  regresar(){
+    this.modalCtrl.dismiss();
+  }
+
+  favorito(){
+
   }
 
 }
