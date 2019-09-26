108be055e6f0201eb597352297d51bb25deb5369
Juan Camilo <jcamilo.osorio15@gmail.com>
2019-09-25 22:01:33 -0500

    design adjustment

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { ImagenPipe } from './imagen.pipe';
 import { ParesPipe } from './pares.pipe';
+import { FiltroImagenPipe } from './filtro-imagen.pipe';
 
 @NgModule({
   declarations: [
     ImagenPipe, 
-    ParesPipe
+    ParesPipe, 
+    FiltroImagenPipe,
   ],
   exports: [
     ImagenPipe,
-    ParesPipe
+    ParesPipe,
+    FiltroImagenPipe
   ]
 
 })
